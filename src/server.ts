b21a3274b7d23f8de51bd17afe619aa837768f44
Anderson Teala <andersonteala@gmail.com>
2023-06-01 22:01:43 -0300

    Commit app

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,44 @@ const app = Fastify()
 const prisma = new PrismaClient()
 
 app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-    // where: {
-    //   title: {
-    //     startsWith: ''
-    //   }
-    // }
-  })
-
+  const habits = await prisma.habit.findMany()
   return habits
 })
+
+const opts = {}
+app.post('/gravar_titulo', opts, async (request, reply) => {
+
+  const teste = await gravrTitulo(request.body)
+
+  if(teste){
+    return {
+      success: true,
+      message: 'Gravado com sucesso'
+    }
+  }else{
+    return {
+      success: false,
+      message: 'Ocorreu um erro'
+    }
+  }
+
+})
+
+async function gravrTitulo(objetoRecebido) {
+
+  objetoRecebido.created_at = new Date();
+
+  try {
+    const user = await prisma.habit.create({
+      data: objetoRecebido
+    })
+    return true;
+  } catch (error) {
+    console.log(error)
+  }
+
+  return false
+}
 
 const port = {
   port: 3333
